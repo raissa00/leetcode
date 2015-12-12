8d36d61b72ba840b845953814692781fd442ddef
dandan Tan <429886109@qq.com>
2015-12-13 00:06:46 +0800

    first commit 283 Move Zeroes

---
+++ #283_MoveZeroes/MoveZeroes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class MoveZeroes_283
+{
+    public static void MoveZeroes(int[] nums)
+    {
+        int numLen = nums.Length;
+        int j = 0;
+        for (int i = 0; i < numLen; i++)
+        {
+            int numi = nums[i];
+            if (numi != 0)
+            {
+                nums[j++] = numi;
+            }
+        }
+        for (; j < numLen; j++)
+            nums[j] = 0;
+    }
+}
